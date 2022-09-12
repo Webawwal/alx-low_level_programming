f4c79b0b3ca8f06eae4ef5b6d64b43acfaacab10
Webawwal <adekunlemoh123@gmail.com>
2022-09-12 16:57:12 -0700

    alx work of 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,27 +10,26 @@
  * Return: Alaways 0 (Success)
  */
 int main(void)
+{
 	int n;
 	int e;
 
-	srabnd(time(0));
-	n = rand() -RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 	e = n % 10;
 
 	if (e > 5)
-        {
-		printf("Last digit %d is %d and is ", n, e);
+	{
+		printf("Last digit of %d is %d and is ", n, e);
 		printf("greater than 5\n");
-	} else if (e == 0)
-        {
+	} else	if (e == 0)
+	{
 		printf("Last digit of %d is %d and is 0\n", n, e);
-	} esle if ((e < 6) && (e != 0))
-{
-	        printf("Last digit of %d is %d and is ", n, e);
+	} else if ((e < 6) && (e != 0))
+	{
+		printf("Last digit of %d is %d and is ", n, e);
 		printf("less than 6 and not 0\n");
+	}
+	return (0);
 }
- return (0);
-
-
-
