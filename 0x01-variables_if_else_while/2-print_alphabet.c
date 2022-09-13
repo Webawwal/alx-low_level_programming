c7e16803f30b1d5afb0e6a283bb129ec676a54d0
Webawwal <adekunlemoh123@gmail.com>
2022-09-12 17:33:35 -0700

    alx work of 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: prints all lowercase alphabets with newline
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch == 'a'; ch <= 'z'; ch++)
+		putcher(ch);
+	putcher('\n');
+	return (0);
+}
