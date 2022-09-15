48ae4faada27a6c18c8165dbdb11586e1b26aeba
Webawwal <adekunlemoh123@gmail.com>
2022-09-15 09:14:42 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,20 @@
 
 /**
  * main - Entry point
+ *
  * print_1-alphabet.c - Entry point
- *
- *
- * Description: program that prints the 1-alphabet.c
  * followed by a new line
  *
- *
- *
- *Always return 0 (Success)
+ * Return: Exit int function with an int 0
+ */
+void print_alphabet(void)
+{
+	int aj;
+
+	for (aj = 'a'; aj <= 'z'; aj++)
+	{
+		_putchar(aj);
+	}
+	_putchar('\n');
+
+}
