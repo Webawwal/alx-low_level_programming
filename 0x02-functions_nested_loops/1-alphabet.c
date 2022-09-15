7615f61addea250a88e71c12755c731dcd3bed1c
Webawwal <adekunlemoh123@gmail.com>
2022-09-14 20:51:53 -0700

    3-islower.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,23 +2,12 @@
 
 /**
  * main - Entry point
- * print_alphabet - Entry point
+ * print_1-alphabet.c - Entry point
  *
- * Description: print alphabets
- * Description: program that prints the alphabets
+ *
+ * Description: program that prints the 1-alphabet.c
  * followed by a new line
  *
- * Return: Always return 0 (Success)
- * Return: Exit int function with an int 0
- */
-void print_alphabet(void)
-{
-	int aj;
-
-	for (aj = 'a'; aj <= 'z'; aj++)
-	{
-		_putchar(aj);
-	}
-	_putchar('\n');
-
-}
+ *
+ *
+ *Always return 0 (Success)
