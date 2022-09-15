f0646bc7afed9a8ad5465c8834716af18572c81d
Webawwal <adekunlemoh123@gmail.com>
2022-09-14 18:12:10 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ * print_alphabet - Entry point
+ *
+ * Description: print alphabets
+ * Description: program that prints the alphabets
+ * followed by a new line
+ *
+ * Return: Always return 0 (Success)
+ * Return: Exit int function with an int 0
+ */
+void print_alphabet(void)
+{
